9c00bf988cf56dd4f222370040a50a132b161699
Stash <Stash_Kempinski@hotmail.com>
2017-01-20 20:19:18 +0100

    Added player script (Duck.cs)

---
+++ UnityProject/Assets/Source/Scripts/Duck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck : MonoBehaviour {
+
+    private AirConsoleManager.Player airController;
+    private float angle = 0;
+    public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void GoLeft()
+    {
+        angle -= speed * Time.deltaTime;
+    }
+
+    private void GoRight()
+    {
+        angle += speed * Time.deltaTime;
+    }
+}
