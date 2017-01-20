73262903319a821158beee72d748e5247b6934a8
Stash <Stash_Kempinski@hotmail.com>
2017-01-20 20:52:54 +0100

    Prefabs folder. Global config.

--- UnityProject/Assets/Source/Scripts/Duck.cs
+++ UnityProject/Assets/Source/Scripts/Duck.cs
@@ -5,27 +5,38 @@ using UnityEngine;
 public class Duck : MonoBehaviour {
 
     private AirConsoleManager.Player airController;
+    public int playerId;
     private float angle = 0;
-    public float speed = 1;
-
+    [Range(0,1)]
+    public float distance = 1.0f;
 	// Use this for initialization
 	void Start () {
-		
+        airController = AirConsoleManager.Instance.GetPlayer(playerId);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        distance -= DuckGameGlobalConfig.distanceSpeed * Time.deltaTime;
+
+        if (airController.GetButtonDown(InputAction.Gameplay.MoveLeft))
+        {
+            GoLeft();
+        }
+
+        if (airController.GetButtonDown(InputAction.Gameplay.MoveRight))
+        {
+            GoRight();
+        }
 	}
 
 
     private void GoLeft()
     {
-        angle -= speed * Time.deltaTime;
+        angle -= DuckGameGlobalConfig.moveSpeed * Time.deltaTime;
     }
 
     private void GoRight()
     {
-        angle += speed * Time.deltaTime;
+        angle += DuckGameGlobalConfig.moveSpeed * Time.deltaTime;
     }
 }
---
+++ UnityProject/Assets/Source/Scripts/DuckGameGlobalConfig.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckGameGlobalConfig {
+    public static float distanceSpeed = 0.1f;
+    public static float moveSpeed = 0.5f;
+}
