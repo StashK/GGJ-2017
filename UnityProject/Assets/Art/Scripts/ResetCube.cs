4d202c75fec69f2f2ec021e5794ab0cec7f5a0a2
wolfvanveen <wolfvanveen@gmail.com>
2017-01-20 22:25:45 +0100

    ART
    ART

---
+++ UnityProject/Assets/Art/Scripts/ResetCube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetCube : MonoBehaviour {
+
+    Rigidbody rigB;
+    Transform trans;
+    
+	// Use this for initialization
+	void Start () {
+        rigB = GetComponent<Rigidbody>();
+        trans = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(rigB.position.y < -5)
+        {
+            float scaler = Random.Range(0.5f, 3.0f);
+            trans.localScale = new Vector3(1.0f * scaler, 1.0f * scaler, 1.0f * scaler);
+            rigB.mass = scaler;
+            rigB.velocity = Vector3.zero;
+            rigB.MovePosition(new Vector3(Random.Range(5, 20), Random.Range(20, 30), Random.Range(0, 30)));
+        }
+	}
+}
