94f9ea6ab9203c350e55082e4abb474b53e4fded
wolfvanveen <wolfvanveen@gmail.com>
2017-01-21 06:05:59 +0100

    DOLPHINS

--- UnityProject/Assets/Art/Scripts/rotate_around.js
+++ UnityProject/Assets/Art/Scripts/rotate_around.js
@@ -5,6 +5,8 @@ var speed=2.0f;
 var x=0.0f;
 var y=1.0f;
 var z=0.0f;
+var ymin=0.1f;
+var ymax=1.5f;
 
 function Start()
 {
@@ -14,7 +16,7 @@ function Start()
 function Update ()
 {
    // x = Random.Range(-0.1,0.1);
-    y = Random.Range(0.1,1.5);
+    y = Random.Range(ymin,ymax);
   //  z = Random.Range(-0.1,0.1);
 
     transform.RotateAround(bird.transform.position, new Vector3(x, y, z), 100 * Time.deltaTime*speed);
