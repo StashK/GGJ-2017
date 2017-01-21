fc8fcac7ff978f3b934ea223bff212a8f1e41bb8
wolfvanveen <wolfvanveen@gmail.com>
2017-01-21 22:29:53 +0100

    wiggle wiggle wiggle

---
+++ UnityProject/Assets/Art/Scripts/wiggle.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var bird:GameObject;
+var speed=1.0f;
+var x=0.0f;
+var y=.0f;
+var z=0.0f;
+var xmin=0.0f;
+var xmax=0.0f;
+var ymin=0.0f;
+var ymax=0.0f;
+var zmin=0.0f;
+var zmax=0.0f;
+
+function Start()
+{
+
+}
+
+function Update ()
+{
+    y = Random.Range(ymin,ymax);
+
+    transform.RotateAround(bird.transform.position, new Vector3(x, y, z), 100 * Time.deltaTime*speed);
+}
