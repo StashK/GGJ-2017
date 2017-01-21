06caae5918b26ed6db49cde46f8bf344c84de72f
crashkonijn <peter_klooster@hotmail.com>
2017-01-21 15:17:23 +0100

    Mesh Gen multithreaded

--- UnityProject/Assets/Source/Scripts/MeshGen.cs
+++ UnityProject/Assets/Source/Scripts/MeshGen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using CielaSpike;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,20 +14,35 @@ public class MeshGen : MonoBehaviour {
     public int sizeY = 5;
     public float size = 1f;
 
+    public float distance = 25f;
+    public static float curDistance = 25f;
+
 	// Use this for initialization
 	void Start () {
         CreateMesh(sizeX, sizeY, size);
-	}
+
+        this.StartCoroutineAsync(Test());
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        
+
+
+    }
+
+    IEnumerator Test ()
+    {
+        curDistance = distance;
+
         int vIndex = 0;
         for (int i = 0; i < sizeX; i++)
         {
             for (int j = 0; j < sizeX; j++)
             {
-                Quad q = quads[i,j];
-                //q.Calculate();
+                Quad q = quads[i, j];
+                q.Calculate();
 
                 int triangleIndex = vIndex;
                 // copy vertices
@@ -35,15 +51,18 @@ public class MeshGen : MonoBehaviour {
                     vertices[vIndex] = q.vertices[k];
                     vIndex++;
                 }
-                
+
                 // set triangles
                 //vertices[triangleIndex] = triangleIndex;
             }
         }
 
+        yield return Ninja.JumpToUnity;
+
+        GetComponent<MeshFilter>().mesh.Clear();
         GetComponent<MeshFilter>().mesh.vertices = vertices;
 
-
+        this.StartCoroutineAsync(Test());
     }
 
     void CreateMesh (int sizeX, int sizeY, float size = 1f)
@@ -58,14 +77,14 @@ public class MeshGen : MonoBehaviour {
         uv = new Vector2[totalGrids * 3 * 2];
         quads = new Quad[sizeX, sizeY];
 
-        //Vector3 center = new Vector3();
+        Vector3 center = new Vector3(sizeX / 2f * size, 0f, sizeY / 2f * size);
 
         int vIndex = 0;
         for (int i = 0; i < sizeX; i++)
         {
             for (int j = 0; j < sizeX; j++)
             {
-                Quad q = new Quad((new Vector3(i, 0, j) * size), new Vector3(i + 1, 0, j + 1) * size);
+                Quad q = new Quad((new Vector3(i, 0, j) * size) - center, (new Vector3(i + 1, 0, j + 1) * size) - center);
 
                 int triangleIndex = vIndex;
                 // copy vertices
@@ -83,11 +102,6 @@ public class MeshGen : MonoBehaviour {
             }
         }
 
-        for (int i = 0; i < m.vertices.Length -1; i++)
-        {
-            Debug.DrawLine(vertices[i], vertices[i + 1], Color.green, 1f);
-        }
-
         m.vertices = vertices;
         m.triangles = triangles;
         m.uv = uv;
@@ -99,9 +113,12 @@ public class MeshGen : MonoBehaviour {
     public class Quad
     {
         public bool active = true;
-        public bool active1 { get { return Random.value > 0f; } }
-        public bool active2 { get { return Random.value > 0f; } }
+        public bool active1 { get { return center1.magnitude < MeshGen.curDistance; } }
+        public bool active2 { get { return center2.magnitude < MeshGen.curDistance; } }
+        public Vector3 center1;
+        public Vector3 center2;
 
+        private Vector3[] originalVertices = new Vector3[6];
         public Vector3[] vertices = new Vector3[6];
         public Vector2[] uv = new Vector2[6];
 
@@ -114,6 +131,10 @@ public class MeshGen : MonoBehaviour {
             this.end = end;
 
             Calculate(start, end);
+
+            originalVertices = (Vector3[])vertices.Clone();
+            center1 = (originalVertices[0] + originalVertices[1] + originalVertices[2]) / 3f;
+            center2 = (originalVertices[3] + originalVertices[4] + originalVertices[5]) / 3f;
 
             for (int i = 0; i < 6; i++)
             {
