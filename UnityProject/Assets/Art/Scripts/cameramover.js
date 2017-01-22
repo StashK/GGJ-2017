08b00c28c821d00cb2150f001bc17aa30c958627
wolfvanveen <wolfvanveen@gmail.com>
2017-01-22 02:52:26 +0100

    moving camera

---
+++ UnityProject/Assets/Art/Scripts/cameramover.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+ 
+function Start()
+{
+
+    LeanTween.moveX( gameObject, 0, 2).setEase( LeanTweenType.easeInOutSine ).setDelay(3f);
+    LeanTween.moveY( gameObject, 100, 2).setEase( LeanTweenType.easeInOutSine ).setDelay(3f);
+    LeanTween.moveZ( gameObject, -25, 2).setEase( LeanTweenType.easeInOutSine ).setDelay(3f);
+    
+    LeanTween.rotateX( gameObject, 70, 2).setEase( LeanTweenType.easeInOutQuad ).setDelay(3f);
+    LeanTween.rotateY( gameObject, 0, 2).setEase( LeanTweenType.easeInOutQuad ).setDelay(3f);
+    LeanTween.rotateZ( gameObject, 0, 2).setEase( LeanTweenType.easeInOutQuad ).setDelay(3f);
+
+}
+ 
+
+function Update () {
+	
+}
