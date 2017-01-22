91b52a791304ff85a399110f605b1f587298a0d4
crashkonijn <peter_klooster@hotmail.com>
2017-01-22 11:38:01 +0100

    zXczXC

---
+++ UnityProject/Assets/Source/Scripts/PixelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PixelController : MonoBehaviour {
+
+    public RawImage image;
+    public static PixelController Instance;
+    public float targetAlpha = 1f;
+
+	// Use this for initialization
+	void Awake () {
+        Instance = this;
+        StartCoroutine(Go());
+	}
+
+    IEnumerator Go ()
+    {
+        yield return new WaitForSeconds(2f);
+        SetTargetAlpha(0f);
+    }
+
+
+    public static void SetTargetAlpha (float f)
+    {
+        Instance.targetAlpha = f;
+        LeanTween.alpha(Instance.image.rectTransform, f, 1f);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        //image.color = new Vector4(1f, 1f, 1f, Mathf.Lerp(image.color.a, targetAlpha, Time.deltaTime));
+	}
+}
